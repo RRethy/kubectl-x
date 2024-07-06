e263ffd947a67ce229ce5c2f3e924519f98e8b1e
Adam P. Regasz-Rethy <adam.regaszrethy@shopify.com>
2024-07-05 23:07:14 -0400

    Initial commit

---
+++ cmd/root.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use: "kubectl-x",
+	Annotations: map[string]string{
+		cobra.CommandDisplayNameAnnotation: "kubectl x",
+	},
+	Short: "kubectl (kube-control) plugin for various extensions I use.",
+	Run: func(cmd *cobra.Command, args []string) {
+		checkErr(cmd.Help())
+	},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, color.RedString("Error:"), err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/RRethy/kubectl-x/cmd"
+
+func main() {
+	cmd.Execute()
+}
