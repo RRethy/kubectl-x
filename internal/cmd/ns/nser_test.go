c3a183c5c34ff953219dc7a3d871a137e452671d
Adam P. Regasz-Rethy <adam.regaszrethy@shopify.com>
2024-07-07 19:31:11 -0400

    Add tests for `k x ns`

---
+++ internal/cmd/ns/nser_test.go
@@ -0,0 +1,68 @@
+package ns
+
+import (
+	"bytes"
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/cli-runtime/pkg/genericiooptions"
+
+	fzf "github.com/RRethy/kubectl-x/internal/fzf/testing"
+	kubeconfig "github.com/RRethy/kubectl-x/internal/kubeconfig/testing"
+	kubernetes "github.com/RRethy/kubectl-x/internal/kubernetes/testing"
+)
+
+func TestNser_Ns(t *testing.T) {
+	tests := []struct {
+		name        string
+		initialNs   string
+		selectedNs  string
+		expectedOut string
+		err         bool
+	}{
+		{
+			name:        "switches namespace successfully",
+			initialNs:   "fo",
+			selectedNs:  "foobar",
+			expectedOut: "Switched to namespace \"foobar\".\n",
+			err:         false,
+		},
+		{
+			name:       "returns error when selecting namespace fails",
+			initialNs:  "fo",
+			selectedNs: "",
+			err:        true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			out := &bytes.Buffer{}
+			err := Nser{
+				KubeConfig: kubeconfig.NewFakeKubeConfig(nil, "foobar", test.selectedNs),
+				IoStreams:  genericiooptions.IOStreams{Out: out},
+				K8sClient: kubernetes.NewFakeClient(map[string][]any{
+					"namespace": {
+						&corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "foo"}},
+						&corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "bar"}},
+						&corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "baz"}},
+					},
+				}),
+				Fzf: fzf.NewFakeFzf([]fzf.InputOutput{
+					{Input: test.initialNs, Output: test.selectedNs},
+				}),
+			}.Ns(context.Background(), test.initialNs)
+
+			if test.err {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				assert.Equal(t, test.expectedOut, out.String())
+			}
+		})
+	}
+}
